9e525c5676a11fe85b2b215582a04a8c34cf7d5a
alice0330ting <136232935+alice0330ting@users.noreply.github.com>
2024-01-04 17:19:04 +0800

    Add files via upload

---
+++ shop-store/js/all.js
@@ -0,0 +1,43 @@
+// 自己寫的 js
+// header、enjoyUs、footer.html 頁面、scroll-top-icon 載入
+let header = document.getElementById('header');
+axios.get('../components/header.html').then(function (res) {
+    header.innerHTML = res.data;
+})
+let enjoyUs = document.getElementById('enjoy-us');
+axios.get('https://alice0330ting.github.io/portfolio/shop-store/components/enjoy-us.html').then(function (res) {
+    enjoyUs.innerHTML = res.data;
+})
+let footer = document.getElementById('footer');
+axios.get('https://alice0330ting.github.io/portfolio/shop-store/components/footer.html').then(function (res) {
+    footer.innerHTML = res.data;
+})
+let scrollTopIcon = document.getElementById('scroll-top-icon');
+axios.get('https://alice0330ting.github.io/portfolio/shop-store/components/scroll-top-icon.html').then(function (res) {
+    scrollTopIcon.innerHTML = res.data;
+})
+
+
+
+
+    // bootstrap 
+    // 登入驗證 js
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })();
